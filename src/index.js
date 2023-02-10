e8894f92243a3683ea32b59d92490eb1af4ca9c8
Borakarayel <m.bora.karayel@gmail.com>
2023-02-10 17:12:37 +0300

    img update & webpack

--- src/index.js
+++ src/index.js
@@ -204,6 +204,9 @@ const displayAbout = () => {
     about.style.fontStyle = "italic";
     about.style.color = "brown";
     about.style.fontSize = "25px";
+    about.style.border= "15px";
+    about.style.borderStyle= "ridge";
+    about.style.borderColor= "brown";
     about.style.backgroundColor = "rgba(250, 235, 215, 0.500)";
     about.style.padding = "150px";
     content.appendChild(about);
@@ -230,4 +233,4 @@ const about = () => {
     navListeners();
 }
 
-menu();
+home();
